ba1011292caf994c9a61eae21529850aaa48d611
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-14 10:56:09 +0700

    adding_white_space

---
+++ 6striping_name_adding_white_space/striping_name_adding_white_space.ts
@@ -0,0 +1,5 @@
+let person_name: string = "\t\n      Mohammad Mahmood"
+
+// Now need to out the following as assignment suggest Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log(`"Person Name with spacing at start will be shwon as we use \t\n and given space=>" ${person_name}`)
