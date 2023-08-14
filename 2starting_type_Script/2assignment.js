462218df6827a2ac2ea238365594d76730a63623
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-14 10:07:43 +0700

    Getting started typescript

---
+++ 2starting_type_Script/2assignment.js
@@ -0,0 +1,4 @@
+// Storing the person's name in a variable
+var personName = "Eric";
+// Printing the message with the person's name
+console.log("Hello ".concat(personName, ", would you like to learn some TypeScript today?"));
---
+++ 2starting_type_Script/2assignment.ts
@@ -0,0 +1,5 @@
+// Storing the person's name in a variable
+let personName: string = "Eric";
+
+// Printing the message with the person's name
+alert(`Hello ${personName}, would you like to learn some TypeScript today?`);
