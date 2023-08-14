6e989db11c282fc28f676db9fa7078dfa821f05d
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-14 11:07:26 +0700

    array futher

---
+++ 13own_array/own_array_just_long_Sample.js
@@ -0,0 +1,5 @@
+var vehicals = ['honda', 'car', 'truck', 'helicopter'];
+console.log("\u201CI would like to own a ".concat(vehicals[0], " motorcycle.\u201D"));
+console.log("\u201CI love to drive a ".concat(vehicals[1], " of BMW\u201D"));
+console.log("\u201CI don't know how to drive a ".concat(vehicals[2], ".\u201D"));
+console.log("\u201CI wish to ride on ".concat(vehicals[3], " its fly high in air.\u201D"));
---
+++ 13own_array/own_array_just_long_Sample.ts
@@ -0,0 +1,6 @@
+var vehicals: any = ['honda', 'car', 'truck', 'helicopter']
+
+console.log(`“I would like to own a ${vehicals[0]} motorcycle.”`);
+console.log(`“I love to drive a ${vehicals[1]} of BMW”`);
+console.log(`“I don't know how to drive a ${vehicals[2]}.”`);
+console.log(`“I wish to ride on ${vehicals[3]} its fly high in air.”`);
