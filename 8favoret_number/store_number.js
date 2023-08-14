c8f0f3d94ccbf25149c68787c20190dce5f50d71
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-14 10:58:22 +0700

    8 to 10 faveriot numbers

---
+++ 8favoret_number/store_number.js
@@ -0,0 +1,2 @@
+var favorite_number = 7;
+console.log("Let display my favorite number ".concat(favorite_number));
---
+++ 8favoret_number/store_number.ts
@@ -0,0 +1,3 @@
+var favorite_number: number = 7;
+
+console.log(`Let display my favorite number ${favorite_number}`);
