0611155693c7b6ba2573aa50b002d06c9a3ae839
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-14 10:59:39 +0700

    array of string and call

---
+++ 12_calling_array_with_string/25alien_color/25alien_color.ts
@@ -0,0 +1,10 @@
+var alien_color: string[] = ['green', 'yellow', 'red']
+
+// passed program showing output
+if(alien_color[0] == "green"){
+    console.log("Player Must Earn 5 Points");
+}
+// failed program does not show output
+if(alien_color[1] == "green"){
+    console.log("Player Must Earn 5 Points");
+}
---
+++ 12_calling_array_with_string/array_and_string.js
@@ -0,0 +1,2 @@
+var names = ['Mahmood', 'Ali'];
+console.log("My name is ".concat(names[0], ", and my best friend is ").concat(name[1]));
