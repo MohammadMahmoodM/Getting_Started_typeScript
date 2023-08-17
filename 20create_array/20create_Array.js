a39fa7e676d2e9ab0c64b49279c65af99e88fd53
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-18 04:20:12 +0700

    creating array though functions

---
+++ 20create_array/20create_Array.js
@@ -0,0 +1,14 @@
+function main() {
+    var proviences = [
+        'punjab',
+        'Kpk',
+        'Baloschistan',
+        'sindh'
+    ];
+    console.log('These are provience', provience);
+    for (var _i = 0, proviences_1 = proviences; _i < proviences_1.length; _i++) {
+        var provience = proviences_1[_i];
+        console.log(provience);
+    }
+}
+main();
---
+++ 20create_array/20create_Array.ts
@@ -0,0 +1,14 @@
+function main():void{
+    const proviences: string[] = [
+        'punjab',
+        'Kpk',
+        'Baloschistan',
+        'sindh'
+    ]
+    console.log('These are provience',provience);
+    for (const provience of proviences){
+        console.log(provience); 
+    }
+}
+
+main();
