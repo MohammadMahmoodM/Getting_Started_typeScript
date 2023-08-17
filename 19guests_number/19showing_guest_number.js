34ffde3b5c5476fff1c4587c5f32ba3f935ba3c0
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-18 04:18:17 +0700

    array length check

---
+++ 19guests_number/19showing_guest_number.js
@@ -0,0 +1,2 @@
+var guest_list = ["mahmood", "usman", "ali", "aisha"];
+console.log("Number of guest", guest_list.length);
---
+++ 19guests_number/19showing_guest_number.ts
@@ -0,0 +1,3 @@
+var guest_list: string[] = ["mahmood","usman","ali","aisha"]
+
+console.log("Number of guest",guest_list.length);
