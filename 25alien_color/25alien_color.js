730f4b7e3b3a92916eef8dd247756a07797213ef
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-26 10:16:22 +0700

    just if statement one true and one failed conditions

---
+++ 25alien_color/25alien_color.js
@@ -0,0 +1,9 @@
+var alien_color = ['green', 'yellow', 'red'];
+// passed program showing output
+if (alien_color[0] == "green") {
+    console.log("Player Must Earn 5 Points");
+}
+// failed program does not show output
+if (alien_color[1] == "green") {
+    console.log("Player Must Earn 5 Points");
+}
---
+++ 25alien_color/25alien_color.ts
@@ -0,0 +1,11 @@
+// let gamer earn point if color is green then 5 and let program failed
+var alien_color: string[] = ['green', 'yellow', 'red']
+
+// passed program showing output
+if(alien_color[0] == "green"){
+    console.log("Player Must Earn 5 Points");
+}
+// failed program does not show output
+if(alien_color[1] == "green"){
+    console.log("Player Must Earn 5 Points");
+}
