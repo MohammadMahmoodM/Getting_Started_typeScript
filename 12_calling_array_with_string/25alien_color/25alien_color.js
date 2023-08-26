400baacf02883be6f684f6870470d6f17d3a855b
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-26 09:17:53 +0700

    just updated

--- 12_calling_array_with_string/25alien_color/25alien_color.js
+++
@@ -1,9 +0,0 @@
-var alien_color = ['green', 'yellow', 'red'];
-// passed program showing output
-if (alien_color[0] == "green") {
-    console.log("Player Must Earn 5 Points");
-}
-// failed program does not show output
-if (alien_color[1] == "green") {
-    console.log("Player Must Earn 5 Points");
-}
