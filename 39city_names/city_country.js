efc856061a622862e77629b2f6051f255af77f5e
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-26 13:11:00 +0700

    useing function in bit advance way in typescript

---
+++ 39city_names/city_country.js
@@ -0,0 +1,8 @@
+//City Names: Write a function called city_country() that takes in the name of a city and 
+//its country. The function should return a string formatted like this: "Lahore, Pakistan"
+function city_country(city, country) {
+    console.log("\"".concat(city, "\", \"").concat(country, "\""));
+}
+city_country("Lahour", "Pakistan");
+city_country("Berlin", "Germany");
+city_country("New Yok", "America");
