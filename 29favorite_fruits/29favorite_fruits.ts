137c2413d3ebb55b3f8724a1f0b48cc167e937a8
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-26 11:38:31 +0700

    how to get value from array, this program giving sample

---
+++ 29favorite_fruits/29favorite_fruits.ts
@@ -0,0 +1,25 @@
+// make and array have fruit names, if array call array with indes if value match in if statment
+// then print the name i really like "That fruit name"
+
+let favorite_fruits: string[] = ["Mango", "Banana", "PineApple"]
+
+if (favorite_fruits[2] == "PineApple"){
+    console.log(`I really like ${favorite_fruits[2]}`); 
+}
+// checking false condition it must not show result
+if (favorite_fruits[2] == "Mango"){
+    console.log(`I really like ${favorite_fruits[2]}`); 
+}
+
+// true checking
+if (favorite_fruits[0] == "Mango"){
+    console.log(`I really like ${favorite_fruits[0]}`); 
+}
+//False checking
+if (favorite_fruits[0] == "Banana"){
+    console.log(`I really like ${favorite_fruits[0]}`); 
+}
+// True checking
+if (favorite_fruits[1] == "Banana"){
+    console.log(`I really like ${favorite_fruits[1]}`); 
+}
