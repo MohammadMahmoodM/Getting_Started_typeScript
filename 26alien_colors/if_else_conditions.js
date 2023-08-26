24633516b54afee7d6e43c826d014eac9ca4338b
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-26 11:17:33 +0700

    if statements in typescript

---
+++ 26alien_colors/if_else_conditions.js
@@ -0,0 +1,21 @@
+var alien_color = ['green', 'yellow', 'red'];
+if (alien_color[0] == "green") {
+    console.log("Player Must Earn 5 Points");
+}
+if (alien_color[1] == "yellow") {
+    console.log("Player Must earn 10 Points");
+}
+// one part in which if work
+if (alien_color[0] == "green") {
+    console.log("If Part Player Must Earn 5 Points");
+}
+else {
+    console.log("Else Part Player Must Earn 10 Points");
+}
+// part where else work
+if (alien_color[0] == "yellow") {
+    console.log("If Part Player Must Earn 5 Points");
+}
+else {
+    console.log("Else Part Player Must Earn 10 Points");
+}
---
+++ 26alien_colors/if_else_conditions.ts
@@ -0,0 +1,24 @@
+//Write a program if aligen color is green then player must earn 5 points if its yellow then  player must earn
+// 10 points, and write one programin which if works and write another part where else works
+var alien_color: string[] = ['green', 'yellow', 'red']
+
+if(alien_color[0]== "green"){
+    console.log("Player Must Earn 5 Points");
+}
+if(alien_color[1]=="yellow"){
+    console.log("Player Must earn 10 Points");
+}
+// one part in which if work
+if(alien_color[0]=="green"){
+    console.log("If Part Player Must Earn 5 Points");
+    }
+    else{
+    console.log("Else Part Player Must Earn 10 Points");
+}
+// part where else work
+if(alien_color[0]=="yellow"){
+    console.log("If Part Player Must Earn 5 Points");
+    }
+    else{
+    console.log("Else Part Player Must Earn 10 Points");
+}
